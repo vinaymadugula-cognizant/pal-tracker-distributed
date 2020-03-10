f168eda323f8d78bfa0026d234bc32e74d38d9a3
Vinay Madugula <VenkataVinayKumar.Madugula@cognizant.com>
2020-03-09 22:28:00 -0500

    OAuth security for service-to-service communication

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -5,17 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
 public class RestConfig {
     @LoadBalanced
-    @Bean
-    public RestOperations restOperations() {
-        return new RestTemplate();
-    }
+
 
     @Bean
     public ObjectMapper objectMapper() {
